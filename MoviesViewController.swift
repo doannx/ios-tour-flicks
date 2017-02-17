1b05b504f3adbf0fd079077fb716e8536482cabe
doannx <doannx@gmail.com>
2017-02-17 22:12:04 +0700

    Add tabbar

--- MoviesViewController.swift
+++ MoviesViewController.swift
@@ -22,6 +22,8 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
     var selectedMovie = NSDictionary()
     
     let refreshControl = UIRefreshControl()
+    var endpoint: String = ""
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -72,7 +74,7 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     func loadJsonData() {
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
-        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let url = URL(string: "https://api.themoviedb.org/3/movie/\(endpoint)?api_key=\(apiKey)")
         let request = URLRequest(
             url: url!,
             cachePolicy: NSURLRequest.CachePolicy.reloadIgnoringLocalCacheData,
